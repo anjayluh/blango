a1007eae34c904c86d95fb52b92c4653fc0dc4fe
Angella Naigaga <anjayluh.wakabi@gmail.com>
2022-03-15 12:35:34 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
+import blango_auth.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index)
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("", blog.views.index),
 ]
